d31b439393de3df2f4a266265e36f71457ba4aff
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-06-14 21:22:57 +0300

    Example

---
+++ OOP-static-vs-instance/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_static_vs_instance
+{
+    public class Person
+    {
+        public string FirstName;
+
+        public string LastName;
+
+        public static int TotalPopulation;
+
+        public const int MaxPopulationCount = 1000;
+
+        public void Print()
+        {
+            //this. pentru membri de instanta
+            //Class. folosit pentru membri statici
+            Console.WriteLine($"Person: {this.FirstName} {this.LastName} {Person.TotalPopulation}");
+        }
+
+        public static void PrintTotalPopulation()
+        {
+            Console.WriteLine($"Total populations is: {Person.TotalPopulation}");
+        }
+
+        public static void PrintPerson(Person p)
+        {
+            Console.WriteLine($"Person is: {p.FirstName} {p.LastName}");
+        }
+    }
+}
--- OOP-static-vs-instance/Program.cs
+++ OOP-static-vs-instance/Program.cs
@@ -6,7 +6,18 @@ namespace OOP_static_vs_instance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person1 = new Person();
+            person1.FirstName = "John";
+            person1.LastName = "Doe";
+            Person.TotalPopulation = 1;
+
+            Person person2 = new Person();
+            person2.FirstName = "Mary";
+            person2.LastName = "Doe";
+            Person.TotalPopulation = 2;
+
+            Console.WriteLine($"Person1: {person1.FirstName} {person1.LastName} {Person.TotalPopulation}");
+            Console.WriteLine($"Person2: {person2.FirstName} {person2.LastName} {Person.TotalPopulation}");
         }
     }
 }
