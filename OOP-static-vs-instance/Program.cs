e50567189516a8c2507c876f2594db8af005309f
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-06-14 19:58:54 +0300

    Add project files.

---
+++ OOP-static-vs-instance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_static_vs_instance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
